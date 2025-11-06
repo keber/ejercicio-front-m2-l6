321064981a466750bf6db8afe2fc5003e436601e
Keber Flores <keberflores@gmail.com>
2025-11-06 19:57:32 -0300

    ✨ feat(ui): add back-to-top button functionality
    - implement a fixed back-to-top button in CSS
    - add scroll detection and smooth scroll functionality in JS
    
    💄 style(navbar): enhance navbar with fixed-top and tertiary background
    
    - make navbar fixed at the top for better accessibility
    - apply bg-body-tertiary class for improved visual consistency
    
    ✨ feat(dependencies): include font-awesome for icon usage
    
    - integrate font-awesome for back-to-top button icon styling

---
+++ assets/js/utils.js
@@ -0,0 +1,27 @@
+// Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+    const scrollOptions = {
+        top: 0, // Scroll to the top of the page
+        left: 0, // Keep the horizontal position the same
+        behavior: 'smooth' // Enable smooth scrolling animation
+    };
+  window.scrollTo(scrollOptions);
+}
